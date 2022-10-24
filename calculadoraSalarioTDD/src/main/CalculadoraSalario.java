1da285396183f7811ce3c55da30daf3839c80c77
Nayara Souza <nayara.souza@pitang.com>
2022-10-24 19:50:43 -0300

    Adicionada implementacao correta da funcao calculaSalario para resultado esperado em CalculadoraTest

--- calculadoraSalarioTDD/src/main/CalculadoraSalario.java
+++ calculadoraSalarioTDD/src/main/CalculadoraSalario.java
@@ -4,6 +4,11 @@ public class CalculadoraSalario {
 
 	public double calculaSalario(Funcionario funcionario) {
 
+		if (funcionario.getCargo() == Cargos.DESENVOLVEDOR) {
+			if(funcionario.getSalario() >= 3000) {
+				return funcionario.getSalario() * 0.80;
+			}
+		}
 		return 0;
 	}
 
