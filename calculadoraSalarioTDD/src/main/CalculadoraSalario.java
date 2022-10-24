83764f405910c4566c867bf77d0900dbebaeae10
Nayara Souza <nayara.souza@pitang.com>
2022-10-24 20:18:38 -0300

    Implementacao de calculo de salario de cargo DBA

--- calculadoraSalarioTDD/src/main/CalculadoraSalario.java
+++ calculadoraSalarioTDD/src/main/CalculadoraSalario.java
@@ -10,6 +10,13 @@ public class CalculadoraSalario {
 			} else {
 				return funcionario.getSalario() * 0.90;
 			}
+		} 
+		else if (funcionario.getCargo() == Cargos.DBA) {
+			if(funcionario.getSalario() >= 2000) {
+				return funcionario.getSalario() * 0.75;
+			} else {
+				return funcionario.getSalario() * 0.85;
+			}
 		}
 		return 0;
 	}
