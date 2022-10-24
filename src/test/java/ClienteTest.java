4c4a5dbd11afb3584bba7fa89d9890d2bc6e7bb9
gildomneto <gildo.neto@ccc.ufcg.edu.br>
2022-10-24 17:26:28 -0300

    primeiro teste criaCliente

---
+++ src/test/java/ClienteTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClienteTest {
+
+    @Test
+    public void CriaClienteTest(){
+        Cliente cliente = new Cliente();
+    }
+}
