33c00fb47a9348700dcddb9864d41d72d96fa85b
Nayara Souza <nayara.souza@pitang.com>
2022-10-24 20:27:58 -0300

    Refatoracao do codigo de calculo salario, passando em todos os testes

--- calculadoraSalarioTDD/src/main/CalculadoraSalario.java
+++ calculadoraSalarioTDD/src/main/CalculadoraSalario.java
@@ -3,36 +3,22 @@ package main;
 public class CalculadoraSalario {
 
 	public double calculaSalario(Funcionario funcionario) {
-
-		if (funcionario.getCargo() == Cargos.DESENVOLVEDOR) {
-			if(funcionario.getSalario() >= 3000) {
-				return funcionario.getSalario() * 0.80;
-			} else {
-				return funcionario.getSalario() * 0.90;
-			}
+		
+		Cargos cargo = funcionario.getCargo();
+		double salario = funcionario.getSalario();
+		
+		switch(cargo) {
+		
+		case DESENVOLVEDOR:
+			return salario >= 3000 ? salario * 0.8 : salario * 0.9;
+		case DBA:
+		case TESTADOR:
+			return salario >= 2000 ? salario * 0.75 : salario * 0.85;
+		case GERENTE:
+			return salario >= 5000 ? salario * 0.7 : salario * 0.8;
+		default:
+			return salario;
+		
 		} 
-		else if (funcionario.getCargo() == Cargos.DBA) {
-			if(funcionario.getSalario() >= 2000) {
-				return funcionario.getSalario() * 0.75;
-			} else {
-				return funcionario.getSalario() * 0.85;
-			}
-		}
-		else if (funcionario.getCargo() == Cargos.TESTADOR) {
-			if(funcionario.getSalario() >= 2000) {
-				return funcionario.getSalario() * 0.75;
-			} else {
-				return funcionario.getSalario() * 0.85;
-			}
-		}
-		else if (funcionario.getCargo() == Cargos.GERENTE) {
-			if(funcionario.getSalario() >= 5000) {
-				return funcionario.getSalario() * 0.70;
-			} else {
-				return funcionario.getSalario() * 0.80;
-			}
-		}
-		return 0;
 	}
-
 }
