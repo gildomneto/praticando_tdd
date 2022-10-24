59e6a1e70ae609e019d02954202eb18c8f342c1c
gildomneto <gildo.neto@ccc.ufcg.edu.br>
2022-10-24 17:59:42 -0300

    falha na existencia do controller de cliente

--- src/test/java/Testes.java
+++ src/test/java/Testes.java
@@ -4,12 +4,17 @@ import java.text.ParseException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ClienteTest {
+class Testes {
 
     @Test
     public void CriaClienteTest() throws ParseException {
         Cliente cliente = new Cliente("Gildo", "24/10/2022", "PB");
 
         assertEquals("Gildo", cliente.getNomeCliente());
+    }
+
+    @Test
+    public void CriaFaturaTest() throws ParseException {
+        String clientGuid = clientController.criaClient("Gildo", "24/10/2022", "PB");
     }
 }
