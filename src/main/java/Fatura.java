a85607650a24184f529e93d389a08de6187a1cdc
gildomneto <gildo.neto@ccc.ufcg.edu.br>
2022-10-24 18:46:00 -0300

    atributos de fatura criados

--- src/main/java/Fatura.java
+++ src/main/java/Fatura.java
@@ -1,4 +1,21 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
 public class Fatura {
 
+    private String codigo;
+    private float valor;
+    private Date data;
+    private String cliente;
+
+    public Fatura(float valor, String data, String cliente) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        this.codigo = UUID.randomUUID().toString();
+        this.valor = valor;
+        this.data = simpleDateFormat.parse(data);
+        this.cliente = cliente;
+    }
 
 }
