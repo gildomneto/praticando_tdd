1e796a71e6782bfbdc0a947c9e69f7eefdb277c7
gildomneto <gildo.neto@ccc.ufcg.edu.br>
2022-10-24 17:44:25 -0300

    argumentos de cliente criados

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,2 +1,21 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
 public class Cliente {
+
+    private UUID idCliente;
+    private String nomeCliente;
+    private Date data;
+    private String estado;
+
+
+    public Cliente(String nomeCliente, String data, String estado) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        this.idCliente = UUID.randomUUID();
+        this.nomeCliente = nomeCliente;
+        this.data = simpleDateFormat.parse(data);
+        this.estado = estado;
+    }
 }
--- src/test/java/ClienteTest.java
+++ src/test/java/ClienteTest.java
@@ -1,11 +1,13 @@
 import org.junit.jupiter.api.Test;
 
+import java.text.ParseException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClienteTest {
 
     @Test
-    public void CriaClienteTest(){
-        Cliente cliente = new Cliente("Gildo", "24/10/2022");
+    public void CriaClienteTest() throws ParseException {
+        Cliente cliente = new Cliente("Gildo", "24/10/2022", "PB");
     }
 }
